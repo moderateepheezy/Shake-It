e25d8090bd431aacc790cfd03ba39e59b181bbb5
moderateepheezy <moderateepheezy@gmail.com>
2016-09-14 13:24:57 +0100

    shake your phone

--- Shake It/ViewController.swift
+++ Shake It/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player = AVAudioPlayer()
+    
+    var sounds = ["sound1", "sound2", "sound3","sound4", "sound5", "sound6", "sound7"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        
+        if event!.subtype == UIEventSubtype.MotionShake{
+            
+            let randomNumber = Int(arc4random_uniform(UInt32(sounds.count)))
+            
+            let fileLocation = NSBundle.mainBundle().pathForResource(sounds[randomNumber], ofType: "mp3")
+            
+            do{
+                try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: fileLocation!))
+                player.play()
+            }catch{
+                
+            }
+        }
+    }
 
 }
 
